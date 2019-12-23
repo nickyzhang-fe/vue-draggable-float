88443661cb400f5bf5145437f946e3115b3af07f
5ibinbin <zhangxia2013105@163.com>
2019-12-23 23:05:09 +0800

    初始化项目

---
+++ PersonalProject/css-practice/README.md
@@ -0,0 +1,10 @@
+# CssTraining
+百度前度技术学院（http://ife.baidu.com/task/all）
+
+  - DayOne：HTML基础和CSS编码
+  - DayTwo：三栏式布局
+  - DayThree：定位和居中问题
+  - DayFour:HTML及CSS编码(二)
+  - DayFive:HTML及CSS模拟报纸排版
+  - DaySix:常见的产品技术官网
+  - DaySeven:响应式栅格布局
---
+++ PersonalProject/css-practice/src/js/index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Cral-Gates on 2016/12/29.
+ */
+// var animation = bodymovin.loadAnimation({
+//     container: document.getElementById('bm'),
+//     renderer: 'svg',
+//     loop: true,
+//     autoplay: true,
+//     path: '../js/blackfish.json'
+// });
